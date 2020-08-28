1885b272cef30971cc319de148cdac4547e68157
JonasNTI <jonas.olanders@elev.ga.ntig.se>
2020-08-28 09:39:02 +0200

    syntax Intro

---
+++ Introkod_syntax/syntax.py
@@ -0,0 +1,13 @@
+print("hej Kokchun")
+
+# skapar en variabel och tilldelar det värdet "Jonas"
+name = "Jonas"
+
+# print("Ditt namn är", name)
+print(f"Ditt namn är {name}")
+
+# float typomvandlar strängen till ett flyttal för att kunna räkna med det
+side = float(input("Ange kvadratens sida:"))
+area = side**2
+
+print(f"Arean av kvadraten är {area} a.e.")
